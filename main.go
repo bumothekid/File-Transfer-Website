5639f05e5687eb643c063d9231de44b4462299fb
David Riegel <40246197+bumothekid@users.noreply.github.com>
2023-12-18 19:45:33 +0100

    Add function to view download page and download file

--- main.go
+++ main.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 type File struct {
 	FileName string
+}
+
+type FileDatabase struct {
+	UniqueID   string `json:"uniqueID"`
+	Path       string `json:"path"`
+	Name       string `json:"name"`
+	UploadTime int64  `json:"uploadTime"`
+	Type       string `json:"type"`
+}
+
+type FileDatabaseJSON struct {
+	Files []FileDatabase `json:"files"`
+}
+
+func generateRandomID(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var database []FileDatabase
+
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	randomID := ""
+
+	for i := 0; i < length; i++ {
+		randomID += string(charset[random.Intn(len(charset))])
+	}
+
+	storage, err := os.Open("storage.json")
+
+	if os.IsNotExist(err) {
+		storage, err = os.Create("storage.json")
+
+		if err != nil {
+			fmt.Println(err)
+			return ""
+		}
+	}
+
+	json.NewDecoder(storage).Decode(&database)
+	defer storage.Close()
+
+	for _, file := range database {
+		if file.UniqueID == randomID {
+			return generateRandomID(length)
+		}
+	}
+
+	return randomID
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -51,9 +101,139 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	storage, err := os.OpenFile("storage.json", os.O_RDWR, 0644)
+
+	if os.IsNotExist(err) {
+		storage, err = os.Create("storage.json")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	randomID := generateRandomID(10)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var fileDatabase map[string]FileDatabase
+
+	json.NewDecoder(storage).Decode(&fileDatabase)
+
+	fmt.Println("Old file database:")
+	fmt.Println(fileDatabase)
+
+	fileDatabase[randomID] = FileDatabase{
+		UniqueID:   randomID,
+		Path:       fmt.Sprintf("storage/%s.%s", randomID, strings.Split(fileHeader.Filename, ".")[1]),
+		Name:       fileHeader.Filename,
+		UploadTime: time.Now().Unix(),
+		Type:       fileHeader.Header.Get("Content-Type"),
+	}
+
+	newFileBytes, err := json.Marshal(fileDatabase)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	storage.WriteAt(newFileBytes, 0)
+
+	defer storage.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("New file bytes:")
+	fmt.Println(newFileBytes)
+
 	template := template.Must(template.ParseFiles("templates/upload.html"))
 	fileHead := File{FileName: fileHeader.Filename}
 	template.Execute(w, fileHead)
+}
+
+// TODO: Implement JSON or SQLite database to store file names and paths with a unique ID for reference and for the url path where to download also save the date and time of upload to delete after x days/hours
+
+func showFilePage(w http.ResponseWriter, r *http.Request) {
+	var database map[string]FileDatabase
+
+	file, err := os.Open("storage.json")
+
+	if os.IsNotExist(err) {
+		file, err = os.Create("storage.json")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	json.NewDecoder(file).Decode(&database)
+	defer file.Close()
+
+	fileURL := strings.Split(r.URL.Path, "/")
+	fileUniqueID := fileURL[len(fileURL)-1]
+
+	if _, ok := database[fileUniqueID]; !ok {
+		http.Error(w, "File not found", http.StatusNotFound)
+
+		// TODO: Redirect to 404 page
+		return
+	}
+
+	fileInfo := database[fileUniqueID]
+
+	template := template.Must(template.ParseFiles("templates/download.html"))
+	template.Execute(w, fileInfo)
+}
+
+func downloadFile(w http.ResponseWriter, r *http.Request) {
+	var database map[string]FileDatabase
+
+	file, err := os.Open("storage.json")
+
+	if os.IsNotExist(err) {
+		file, err = os.Create("storage.json")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	json.NewDecoder(file).Decode(&database)
+	defer file.Close()
+
+	fileURL := strings.Split(r.URL.Path, "/")
+	fileUniqueID := fileURL[len(fileURL)-1]
+
+	if _, ok := database[fileUniqueID]; !ok {
+		http.Error(w, "File not found", http.StatusNotFound)
+
+		// TODO: Redirect to 404 page
+		return
+	}
+
+	fileInfo := database[fileUniqueID]
+
+	file, err = os.Open(fileInfo.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", fileInfo.Type)
+	w.Header().Set("Content-Disposition", "attachment; filename="+fileInfo.Name)
+
+	http.ServeFile(w, r, fileInfo.Path)
+	return
 }
 
 func main() {
@@ -61,6 +241,8 @@ func main() {
 
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/storage/", showFilePage)
+	http.HandleFunc("/download/", downloadFile)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
